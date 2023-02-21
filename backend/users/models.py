ad5750c9b78b9ad59df611c9a2f5c66f635365e9
victini <ivansaulromano100@gmail.com>
2023-02-21 11:49:02 -0300

    deploy django beta

--- backend/users/models.py
+++ backend/users/models.py
@@ -27,7 +27,6 @@ class UserManager(BaseUserManager):
 # requiere el email y el name
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
