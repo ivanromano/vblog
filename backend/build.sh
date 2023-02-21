02ccc659a321a53f940164c25c4c4b5b7c366e0e
victini <ivansaulromano100@gmail.com>
2023-02-21 12:37:40 -0300

    problemasssssss

--- backend/build.sh
+++ backend/build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD', name='Superuser')" | python manage.py shell
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
 fi
  
 python manage.py collectstatic --no-input
