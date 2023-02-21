3bfe1966f5b0c2db8702eb22dcaa215113b848a2
victini <ivansaulromano100@gmail.com>
2023-02-21 12:27:43 -0300

    problema

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,6 @@
+# este es mi archivo de models.py en django. abajo tengo el archivo de build.sh para que al desplegar en render, pueda crear un superusuario. sin embargo en render, el superusuario no existe, no me deja iniciar sesion
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
-
-# Create your models here.
-
 
 class UserManager(BaseUserManager):
 
@@ -27,9 +24,12 @@ class UserManager(BaseUserManager):
 # requiere el email y el name
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, default='poi')  # Agregar un valor predeterminado
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+# este es build.sh, el archivo que mencione antes
