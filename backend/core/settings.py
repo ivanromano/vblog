e88ba97aa55e043ae75880ad1fbac524a473d289
victini <ivansaulromano100@gmail.com>
2023-02-22 13:37:02 -0300

    frontend + backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -90,8 +90,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://localhost:3001',
 
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
+    'https://vblog-ijd8ug6zh-ivanromano.vercel.app/',
+    'https://vblog-ijd8ug6zh-ivanromano.vercel.app',
 
     'http://127.0.0.1:8001',
     'http://localhost:8001',
