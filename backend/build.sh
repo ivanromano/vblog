4310f2e9dcfddd36229c38e2cfe88b40e65c0472
victini <ivansaulromano100@gmail.com>
2023-02-22 17:21:35 -0300

    50000000000000

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
 # if [[ $CREATE_SUPERUSER ]];
 # then
 #   echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
