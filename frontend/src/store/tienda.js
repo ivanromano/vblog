e3219493bda480a5104ba59be843d840d9004868
victini <ivansaulromano100@gmail.com>
2023-02-23 08:30:01 -0300

    deploy front

--- frontend/src/store/tienda.js
+++ frontend/src/store/tienda.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const useDjangoStore = defineStore("django", () => {
 	const token = ref(null)
 	const contenido = ref(null)
-  const localhost = 'http://localhost:8000'
+  const localhost = 'https://poison.onrender.com'
   const post = ref()
   const count = ref()
   const next = ref()
