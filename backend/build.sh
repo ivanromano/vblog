0224f6d3bc3c8a611a4c41740f7c901e44440c9b
victini <ivansaulromano100@gmail.com>
2023-02-23 08:12:51 -0300

    error migraciones

--- backend/build.sh
+++ backend/build.sh
@@ -4,10 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
  
-if [[ $CREATE_SUPERUSER ]];
-then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+# fi
  
 python manage.py collectstatic --no-input
 python manage.py migrate
